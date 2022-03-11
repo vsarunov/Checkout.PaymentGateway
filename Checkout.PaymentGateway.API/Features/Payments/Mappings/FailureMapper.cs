5a49d25b90e8476522667a138c84db2c98ab59f0
Vlad Sarunov <Vlad.sarunov@amido.com>
2022-03-11 12:50:49 +0000

    Configuring failure and status code mappings.

---
+++ Checkout.PaymentGateway.API/Features/Payments/Mappings/FailureMapper.cs
@@ -0,0 +1,25 @@
+﻿using Checkout.PaymentGateway.API.Models.Responses.Payments;
+using Checkout.PaymentGateway.Common.Enums;
+
+namespace Checkout.PaymentGateway.API.Features.Payments.Mappings
+{
+    internal static class FailureMapper
+    {
+        internal static Failure ToResponseFailure(this Domain.Shared.Failure failure)
+        {
+            return new Failure(failure.Id, new Error(failure.ErrorCode.ToString()));
+        }
+
+        internal static int ToStatusCode(this ErrorCode errorCode)
+        {
+            switch (errorCode)
+            {
+                case ErrorCode.PaymentRejected: return StatusCodes.Status400BadRequest;
+                case ErrorCode.PaymentFailed: return StatusCodes.Status500InternalServerError;
+                case ErrorCode.PaymentAlreadyExists: return StatusCodes.Status409Conflict;
+                case ErrorCode.PaymentNotFound: return StatusCodes.Status404NotFound;
+                default: throw new ArgumentOutOfRangeException(nameof(errorCode));
+            }
+        }
+    }
+}
